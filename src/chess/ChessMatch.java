20ab8de5c6570b7bbd7911aecdcf999f57fa7385
Felipe Kerwald <kerwald.s@gmail.com>
2025-04-05 10:45:56 -0300

    melhoria na impressão das peças  no terminal

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,11 +34,22 @@ public class ChessMatch {
         board.placePiece( piece, new ChessPosition(column, row).toPosition());
     }
 
-    private void initialSetup(){
-        placeNewPiece( 'a', 8, new King(board, Color.WHITE));
-        placeNewPiece( 'b', 8, new Rock(board, Color.WHITE));
-        placeNewPiece( 'c', 8, new Rock(board, Color.WHITE));
-        placeNewPiece( 'd', 8, new King(board, Color.WHITE));
+    private void initialSetup() {
+
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rock(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rock(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rock(board, Color.BLACK));
+        placeNewPiece('d', 8, new Rock(board, Color.BLACK));
+        
     }
 
 }
