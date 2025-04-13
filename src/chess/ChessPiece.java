37c7e65dfa8e717502854c5a0f503409875f7a93
Felipe Kerwald <kerwald.s@gmail.com>
2025-04-13 08:48:33 -0300

    Adicionar contagem de movimentos das peças

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,6 +4,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 public abstract class ChessPiece extends Piece {
+
     private Color color;
     private int moveCount;
     
@@ -14,6 +15,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+    
+    public void decreaseMoveCount(){
+        moveCount--;
     }
     
     public ChessPosition getChessPosition(){
